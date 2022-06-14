44c286fc5caf21d9d14b39c550848208220c4ab4
liuchuanan <liuchuanan@xianlai-inc.com>
2022-06-14 11:35:21 +0800

    音频重采样项目添加

--- audio_resample/mainwindow.cpp
+++ audio_resample/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -11,5 +12,12 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+
+void MainWindow::on_audioButton_clicked()
+{
+    _audioThread = new AudioThread(this);
+    _audioThread->start();
 }
 
--- build-audio_resample-Qt_6_2_4_for_macOS-Debug/ui_mainwindow.h
+++ build-audio_resample-Qt_6_2_4_for_macOS-Debug/ui_mainwindow.h
@@ -13,6 +13,7 @@
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -22,6 +23,7 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QPushButton *audioButton;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -32,9 +34,13 @@ public:
         MainWindow->resize(800, 600);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        audioButton = new QPushButton(centralwidget);
+        audioButton->setObjectName(QString::fromUtf8("audioButton"));
+        audioButton->setGeometry(QRect(60, 90, 100, 32));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 24));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -48,6 +54,7 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        audioButton->setText(QCoreApplication::translate("MainWindow", "\351\237\263\351\242\221\351\207\215\351\207\207\346\240\267", nullptr));
     } // retranslateUi
 
 };

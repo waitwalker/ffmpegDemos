dd15c47e5fffd793172d7403fe224f407caf421b
liuchuanan <liuchuanan@xianlai-inc.com>
2022-06-16 11:21:55 +0800

    重采样两个方法封装添加处理

--- audio_resample/audiothread.cpp
+++ audio_resample/audiothread.cpp
@@ -9,6 +9,8 @@ extern "C" {
 }
 // \斜线表示后面东西都是你的
 
+
+
 AudioThread::AudioThread(QObject *parent)
     : QThread{parent}
 {
@@ -30,26 +32,43 @@ AudioThread::~AudioThread() {
 
 void AudioThread::run() {
     // 文件已经准备好了
-    // 文件名
-    const char *inFilename = "/Users/walkerwait/Desktop/4800_f32le.pcm";
+    ResampleAudioSpec in;
+    in.filename = "/Users/walkerwait/Desktop/4800_f32le.pcm";
     // 输入的一些参数
-    AVSampleFormat inSampleFmt = AV_SAMPLE_FMT_FLT;
-    int inSampleRate = 48000;
+    in.sampleFmt = AV_SAMPLE_FMT_FLT;
+    in.sampleRate = 48000;
     // 声道布局
-    int inChLayout = AV_CH_LAYOUT_MONO;// 单声道
+    in.chLayout = AV_CH_LAYOUT_MONO;// 单声道
 
-    const char *outFilename = "/Users/walkerwait/Desktop/44100_s16le.pcm";
-    AVSampleFormat outSampleFmt = AV_SAMPLE_FMT_S16;
-    int outSampleRate = 44100;
+    ResampleAudioSpec out;
+    out.filename = "/Users/walkerwait/Desktop/44100_s16le.pcm";
+    out.sampleFmt = AV_SAMPLE_FMT_S16;
+    out.sampleRate = 44100;
     // 声道布局
-    int outChLayout = AV_CH_LAYOUT_STEREO;// 双声道，立体声
-    FFmpegs::resampleAuido(inFilename,
-                           inSampleRate,
-                           inSampleFmt,
-                           inChLayout,
-                           outFilename,
-                           outSampleRate,
-                           outSampleFmt,
-                           outChLayout);
+    out.chLayout = AV_CH_LAYOUT_STEREO;// 双声道，立体声
+    FFmpegs::resampleAuido(in,
+                           out);
+
+//    // 文件名
+//    const char *inFilename = "/Users/walkerwait/Desktop/4800_f32le.pcm";
+//    // 输入的一些参数
+//    AVSampleFormat inSampleFmt = AV_SAMPLE_FMT_FLT;
+//    int inSampleRate = 48000;
+//    // 声道布局
+//    int inChLayout = AV_CH_LAYOUT_MONO;// 单声道
+
+//    const char *outFilename = "/Users/walkerwait/Desktop/44100_s16le.pcm";
+//    AVSampleFormat outSampleFmt = AV_SAMPLE_FMT_S16;
+//    int outSampleRate = 44100;
+//    // 声道布局
+//    int outChLayout = AV_CH_LAYOUT_STEREO;// 双声道，立体声
+//    FFmpegs::resampleAuido(inFilename,
+//                           inSampleRate,
+//                           inSampleFmt,
+//                           inChLayout,
+//                           outFilename,
+//                           outSampleRate,
+//                           outSampleFmt,
+//                           outChLayout);
 
 }
--- audio_resample/ffmpegs.h
+++ audio_resample/ffmpegs.h
@@ -4,11 +4,26 @@ extern "C" {
 #include <libavformat/avformat.h>
 }
 
+// 重采样相关参数
+typedef struct {
+    // 文件路径
+    const char *filename;
+    // 采样格式
+    AVSampleFormat sampleFmt;
+    // 采样率
+    int sampleRate;
+    // 声道布局
+    int  chLayout;
+} ResampleAudioSpec;
+
 class FFmpegs
 {
 public:
     FFmpegs();
-    // 音频重采样工具方法封装
+    // 音频重采样工具方法封装 两个方法一样
+    static void resampleAuido(ResampleAudioSpec &in,
+                              ResampleAudioSpec &out);
+
     static void resampleAuido(const char *inFilename,
                               int inSampleRate,
                               AVSampleFormat inSampleFmt,
